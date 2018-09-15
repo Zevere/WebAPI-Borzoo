7a31a57d21724a411326bae43d474239f56cfdf3
Poulad <poulad.pld@gmail.com>
2018-09-14 21:46:07 -0400

    Update graphql pkg version

--- ci/deploy/deploy_docker_registry.js
+++ ci/deploy/deploy_docker_registry.js
@@ -4,9 +4,8 @@ require('../logging')
 exports.deploy = function (source, target, user, pass) {
     console.info(`pushing docker local image "${source}" to "${target}"`)
 
-    $.exec('docker logout')
-    $.exec(`docker login --username ${user} --password "${pass}"`)
     $.exec(`docker tag "${source}" "${target}"`)
+    $.exec(`docker login --username ${user} --password "${pass}"`)
     $.exec(`docker push "${target}"`)
     $.exec('docker logout')
 }
