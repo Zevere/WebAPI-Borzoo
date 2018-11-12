ee1c80fba1c952954f086c142503be03968c9cd3
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-11-12 14:08:44 -0500

    Update appveyor tests

--- test/WebAppTests/User Graph.cs
+++ test/WebAppTests/User Graph.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -16,6 +17,43 @@ namespace WebAppTests
         public UserGraphTests(TestsFixture fxt)
         {
             _fxt = fxt;
+        }
+
+        [OrderedFact("Should create a new user with the required fields only")]
+        public async Task Should_Create_New_User()
+        {
+            string mutation = @"mutation SomeMutation($u: UserInput!) {
+                createUser(user: $u) {
+                    id firstName lastName token daysJoined joinedAt lists { id }
+                }
+            }";
+            HttpResponseMessage response = await _fxt.HttpClient.PostJsonGraphqlAsync($@"{{
+                ""query"": ""{mutation.Stringify()}"",
+                ""variables"": {{
+                    ""u"": {{
+                        ""name"": ""POULAD1024"",
+                        ""firstName"": ""Poulad"",
+                        ""passphrase"": ""10-pass_phrase-24""
+                    }}
+                }}
+            }}");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Asserts.JsonEqual(
+                $@"{{
+                    data: {{
+                        createUser: {{
+                            id: ""poulad1024"",
+                            firstName: ""Poulad"",
+                            lastName: null,
+                            daysJoined: 0,
+                            joinedAt: ""{DateTime.Today:yyyy-MM-dd}""
+                        }}
+                    }}
+                }}",
+                responseContent
+            );
         }
 
         [OrderedFact("Should fail when querying non-existing user")]
