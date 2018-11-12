5424106d71f9534e73477853faa0b9e1260469b0
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-11-11 22:03:44 -0500

    Configure Travis-CI using multi-stage builds

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM microsoft/dotnet:2.1-aspnetcore-runtime AS base
+WORKDIR /app/
+
+
+FROM microsoft/dotnet:2.1-sdk AS web-app-build
+ARG configuration=Debug
+WORKDIR /project/
+COPY src src
+RUN dotnet build src/Borzoo.Web/Borzoo.Web.csproj --configuration ${configuration}
+
+
+FROM web-app-build AS publish
+WORKDIR /project/
+RUN dotnet publish src/Borzoo.Web/Borzoo.Web.csproj --configuration Release --output /app/
+
+
+FROM base AS final
+WORKDIR /app/
+COPY src/Borzoo.Data.SQLite/scripts/migrations.sql /var/data/sqlite/
+COPY --from=publish /app /app
+ENV BORZOO_SETTINGS='{"data":{"sqlite":{"migrations":"/var/data/sqlite/migrations.sql"}}}'
+CMD ASPNETCORE_URLS=http://+:${PORT:-80} dotnet Borzoo.Web.dll
+
+
+FROM microsoft/dotnet:2.1-sdk AS solution-build
+ARG configuration=Debug
+WORKDIR /project/
+COPY . .
+RUN dotnet build Borzoo.sln --configuration ${configuration}
--- scripts/build/index.js
+++ scripts/build/index.js
@@ -3,20 +3,61 @@ const path = require('path')
 require('../logging')
 
 $.config.fatal = true
-const root = path.join(__dirname, '..', '..')
-const dist_dir = path.join(root, 'dist')
+const root = path.resolve(`${__dirname}/../..`)
 
-const publish_script = require('./build_web_api')
-const docker_script = require('./build_docker_image')
+function get_docker_options() {
+    const settings_script = require('./../deploy/deploy_settings')
+    let settings
+    try {
+        settings = settings_script.get_deployment_settings()
+    } catch (e) {
+        return
+    }
+
+    let docker_options;
+    for (const prop in settings) {
+        for (const deployment of settings[prop]) {
+            if (deployment.type === 'docker') {
+                docker_options = deployment.options;
+            }
+        }
+    }
+    return docker_options
+}
 
 try {
-    publish_script.clear()
-    publish_script.build_asp_net_core_app()
+    console.info(`building Docker images`)
 
-    docker_script.build_image()
+    $.cd(root)
+
+    console.debug('building the solution with "Release" configuration and "borzoo:solution" tag')
+    $.exec(
+        `docker build --tag borzoo:solution --no-cache --target solution-build --build-arg "configuration=Release" .`
+    )
+
+    console.debug('building the final web app with "borzoo:latest" tag')
+    $.exec(`docker build --tag borzoo --target final .`)
+
+
+    console.debug('reading Docker deployment options')
+    docker_options = get_docker_options()
+    if (docker_options) {
+        console.debug('pushing images to the Docker hub')
+        const docker_deploy_script = require('./../deploy/deploy_docker_registry')
+
+        docker_deploy_script.deploy(
+            'borzoo:solution', 'zevere/webapi-borzoo:unstable-solution', docker_options.user, docker_options.pass
+        )
+
+        docker_deploy_script.deploy(
+            'borzoo:latest', 'zevere/webapi-borzoo:unstable', docker_options.user, docker_options.pass
+        )
+    } else {
+        console.warn('Docker deployment options not found. skipping Docker image push...')
+    }
 } catch (e) {
     console.error(e)
     process.exit(1)
 }
 
-console.info(`Build succeeded: "${path.join(dist_dir, 'app')}"`)
+console.info(`✅ Build succeeded`)
